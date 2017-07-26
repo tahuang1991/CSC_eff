d50d4b3aeb9716f7e82fafe4e27a475a4807fdcc
senka <senka.duric@cern.ch>
2017-07-26 15:24:13 +0200

    new cfg file

---
+++ CSCEfficiency/recoOnlyRun2017B_92X_dataRun2_Prompt_v4_condor.py
@@ -0,0 +1,155 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: RECO -s RAW2DIGI,L1Reco,RECO --runUnscheduled --nThreads 4 --data --era Run2_2017 --scenario pp --conditions 92X_dataRun2_Prompt_v4 --eventcontent RECO --datatier RECO --customise Configuration/DataProcessing/RecoTLR.customisePostEra_Run2_2017 --filein file:/hdfs/store/user/senka/CSC_2017data/1670035E-CD52-E711-AA85-02163E014522.root -n 100 --python_filename=recoOnlyRun2017B_92X_dataRun2_Prompt_v4.py
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process('RECO',eras.Run2_2017)
+HLTProcessName='HLT'
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.Reconstruction_Data_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring($inputFileNames),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.MessageLogger.cerr.FwkReport = cms.untracked.PSet(
+     reportEvery = cms.untracked.int32(1000),
+     limit = cms.untracked.int32(1)
+)
+process.MessageLogger.cerr.default = cms.untracked.PSet(
+     limit = cms.untracked.int32(1)
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('RECO nevts:100'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.RECOoutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('RECO'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    fileName = cms.untracked.string('RECO_RAW2DIGI_L1Reco_RECO.root'),
+    outputCommands = process.RECOEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '92X_dataRun2_Prompt_v4', '')
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.L1Reco_step = cms.Path(process.L1Reco)
+process.reconstruction_step = cms.Path(process.reconstruction)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RECOoutput_step = cms.EndPath(process.RECOoutput)
+
+from RecoMuon.TrackingTools.MuonSegmentMatcher_cff import *
+process.load("TrackingTools.TrackAssociator.DetIdAssociatorESProducer_cff")
+from TrackingTools.TrackAssociator.default_cfi import *
+from RecoMuon.MuonIsolationProducers.trackExtractorBlocks_cff import MIsoTrackExtractorBlock
+
+process.MuonSegmentMatcher = cms.PSet(
+     MatchParameters = cms.PSet(
+         CSCsegments = cms.InputTag("cscSegments"),
+         DTradius = cms.double(0.01),
+         DTsegments = cms.InputTag("dt4DSegments"),
+         RPChits = cms.InputTag("rpcRecHits"),
+         TightMatchCSC = cms.bool(True),
+         TightMatchDT = cms.bool(False)
+     ) 
+)
+
+process.aodDump = cms.EDAnalyzer('TPTrackMuonSys',
+#                                 TrackAssociatorParameterBlock,
+                                 MuonSegmentMatcher,
+                                 TrackExtractor=cms.PSet(MIsoTrackExtractorBlock),
+                                 rootFileName   = cms.untracked.string('CSCPFG_Ineff_DATA_test_1.root'),
+                                 CSCUseTimingCorrections = cms.bool( True ),
+                                 CSCUseGasGainCorrections = cms.bool( True ),
+                                 isMC            = cms.untracked.bool(False),
+                                 saveZ            = cms.untracked.bool(True),
+                                 saveJPsi            = cms.untracked.bool(False),
+                                 mcTag           = cms.untracked.InputTag('genParticles'),
+                                 vertexCollection = cms.InputTag('offlinePrimaryVertices'),
+                                 gTracksTag      = cms.untracked.InputTag('generalTracks'),
+                                 trackProducer   = cms.InputTag('csctfunpacker:'),
+                                 readBadChannels = cms.bool(True),
+                                 readBadChambers = cms.bool(True),
+                                 hltTag      = cms.untracked.InputTag("TriggerResults","",HLTProcessName),
+                                 hltEvTag    = cms.untracked.InputTag("hltTriggerSummaryAOD","",HLTProcessName),
+                                 HLTMuTrgNames =  cms.vstring("HLT_Mu?_v*","HLT_Mu??_v*","HLT_Mu???_v*","HLT_IsoMu?_v*","HLT_IsoMu??_v*","HLT_IsoMu???_v*","HLT_L2Mu?_v*","HLT_L2Mu??_v*","HLT_L2Mu???_v*","HLT_SingleMu*","HLT_L1SingleMu*","HLT_IsoTkMu??_v*","HLT_TkMu??_v*"),
+                                 HLTDiMuTrgName =  cms.string("HLT_DoubleMu?_v*"),
+                                 #                      hltEvTag    = cms.untracked.InputTag("hltTriggerSummaryAOD","","REDIGI36X"),
+                                 L1extraTag   = cms.untracked.InputTag("l1extraParticles"),
+                                 dedxTag         =  cms.untracked.InputTag('dedxHarmonic2'),
+                                 scalersResults = cms.InputTag("scalersRawToDigi","","RECO")
+                                 )
+
+
+process.outputstep = cms.EndPath(process.aodDump)
+
+
+# Schedule definition
+process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.endjob_step,process.outputstep)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+#Setup FWK for multithreaded
+process.options.numberOfThreads=cms.untracked.uint32(4)
+process.options.numberOfStreams=cms.untracked.uint32(0)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.RecoTLR
+from Configuration.DataProcessing.RecoTLR import customisePostEra_Run2_2017 
+
+#call to customisation function customisePostEra_Run2_2017 imported from Configuration.DataProcessing.RecoTLR
+process = customisePostEra_Run2_2017(process)
+
+# End of customisation functions
+#do not add changes to your config after this point (unless you know what you are doing)
+from FWCore.ParameterSet.Utilities import convertToUnscheduled
+process=convertToUnscheduled(process)
+
+
+# Customisation from command line
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
